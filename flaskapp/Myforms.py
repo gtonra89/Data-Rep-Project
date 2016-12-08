358928c573318365a0bea14184cbeb8a1c9a4b30
gtonra89 <gtonra89@users.noreply.github.com>
2016-12-08 14:11:16 +0000

    Update Myforms.py

--- flaskapp/Myforms.py
+++ flaskapp/Myforms.py
@@ -2,5 +2,7 @@ from flask.ext.wtf import Form
 from wtforms.fields import TextField
 from wtforms.validators import Required
 
+#Fields are defined as members on a form in a declarative fashion:
 class TODOFORM(Form):
 	label = TextField('label', validators = [Required()])
+	
