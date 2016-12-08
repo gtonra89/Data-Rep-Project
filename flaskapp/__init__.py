98d684b65e3e8cd8c857786da6a56707181f4735
gtonra89 <gtonra89@users.noreply.github.com>
2016-12-08 13:55:18 +0000

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,8 @@
+#create a Flask instance in __init__.py
 from flask import Flask
+flaskapp = Flask(__name__) 
 
-flaskapp = Flask(__name__)
-flaskapp.config["SECRET_KEY"] = "abc123babyyouandme"
+flaskapp.config["SECRET_KEY"] = "abc123babyyouandme" #configure secret key value
 
-from flaskapp import View
+from flaskapp import View #import the view 
 
